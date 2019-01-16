c8c839c804c9ee7537000e552d373e41b0dc3419
Joel Parkinson <joelparkinson@live.co.uk>
2019-01-16 22:52:43 +0000

    Add BasicBot Project

---
+++ BasicBot/src/Core/Modules/TestCommand.cs
@@ -0,0 +1,32 @@
+﻿using Discord;
+using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicBot.Core.Modules
+{
+    public class TestCommand : ModuleBase<SocketCommandContext>
+    {
+        //Basic Echo Command (Sends whatever the user types after "echo" back into the channel.
+        [Command("Echo")]
+        public async Task Echo([Remainder]string message)
+        {
+            await Context.Message.DeleteAsync();
+            await ReplyAsync(message);
+        }
+
+        //Basic Hello World command but implementing an embed.
+        [Command("Hello")]
+        public async Task Hello()
+        {
+            var embed = new EmbedBuilder()
+                .WithAuthor(Context.Message.Author)
+                .WithColor(Color.Blue)
+                .WithTitle("Hello World Command Example")
+                .WithDescription($"Hello World and {Context.Message.Author}.");
+            await ReplyAsync("", false, embed.Build());
+        }
+    }
+}
---
+++ BasicBot/src/Entities/BotConfig.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicBot.Entities
+{ 
+    //This is the structure of our Config.json file.
+    public class BotConfig
+    {
+        public string DiscordToken { get; set; }
+        public string GameStatus { get; set; }
+    }
+}
